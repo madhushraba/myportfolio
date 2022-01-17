f9cb2a21abf1f46a764eaa1878cb3c17ae958937
parthsavajadiya <parthsavajadiya7443@gmail.com>
2022-01-17 23:28:55 +0530

    addedd

--- src/components/body/projects/project-card.js
+++ src/components/body/projects/project-card.js
@@ -53,7 +53,7 @@ function ProjectCard({ project }) {
           })}
         </div>
       </div>
-      <img src={project.image} className="project-photo" />
+      <img src={project.image} className="project-photo" alt="" />
     </div>
   );
 }
